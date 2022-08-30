82f81804652070fa038814458facce289047d185
antonio <bjelobrka@gmail.com>
2022-08-30 17:20:50 +0200

    Added the rainbow button so taht the divs change their color. Added validation by user input so that the number cant be lower than 1 or greater than 100.

--- external.js
+++ external.js
@@ -1,24 +1,39 @@
 const container = document.querySelector("#container");
 const buttonGrid = document.querySelector("#buttonGrid")
+const buttonRainbow = document.querySelector("#rainbow")
 
-function startGrid(){
+function starterGrid(){
 
-for(let i=0; i<256; i++){
-    const div = document.createElement("div");  
-    container.appendChild(div);
-    div.classList.add("grid");
-    div.classList.add("grid-height");
+    for(let i=0; i<256; i++){
+        const div = document.createElement("div");  
+        container.appendChild(div);
+        div.classList.add("grid");
+        div.classList.add("grid-height");
     
+    }
+    grayColor();
+    rainbow();
 }
-grayColor();
-}
+
 function resizeGrid(){
 
-buttonGrid.addEventListener("click", function(){
+    buttonGrid.addEventListener("click", function(){
 
     let gridSize = prompt("Enter desired square per side(max 100)");
-    let intGridSize = parseInt(gridSize);
+    
+    if(gridSize === null || gridSize === ""){
+        return;
+    }
 
+    let intGridSize = parseInt(gridSize);
+    while(intGridSize > 100 || intGridSize < 0){
+        intGridSize = prompt(gridSize +" is not valid. Please pick a number greater than 0 or lower of 100")
+        
+    }
+
+    if(intGridSize > 100 || gridSize < 0){
+        return;
+    }
 
 
     while (container.lastElementChild) {
@@ -36,6 +51,7 @@ buttonGrid.addEventListener("click", function(){
     }
 
     grayColor();
+    rainbow();
 });
 
 
@@ -52,5 +68,21 @@ function grayColor(){
     
 }
 
-startGrid();
+function rainbow(){
+buttonRainbow.addEventListener("click", () =>{
+    const divs = document.querySelectorAll(".grid")
+    divs.forEach((div) => {
+        div.addEventListener("mouseover", () =>{
+            const randomColor = Math.floor(Math.random()*16777215).toString(16)
+            console.log(randomColor);
+            div.style.backgroundColor = "#" + randomColor;
+        });
+    });
+
+});
+}
+
+
+
+starterGrid();
 resizeGrid();
